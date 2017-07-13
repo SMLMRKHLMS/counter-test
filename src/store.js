5b81607119810878b19d38a4d226595700868ad3
Mark Holmes <smlmrkhlms@gmail.com>
2017-07-13 16:56:21 -0400

    Updated State managment

--- src/store.js
+++ src/store.js
@@ -1,10 +1,7 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
-const store = ({
-  actions,
-  state: initialState
-}) => Component => class Store extends React.Component {
+const store = ({ actions, state: initialState }) => Component => class Store extends React.Component {
 
   state = initialState
 
@@ -14,19 +11,19 @@ const store = ({
   }
 
   getChildContext() {
+
     this.actions = {}
+
     Object.keys(actions).forEach(key => {
-      this.actions[key] = () => this.setState(actions[key])
+      this.actions[key] = (...args) => {
+        this.setState(state => actions[key](state, ...args))
+      }
     })
-    return {
-      actions: this.actions,
-      state: this.state
-    };
+
+    return { actions: this.actions, state: this.state }
   }
 
-  render() {
-    return <Component />
-  }
+  render() { return <Component /> }
 }
 
-export default store;
+export default store
