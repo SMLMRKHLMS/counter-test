d7bd27da8e04b89561763be5bc8f61bd25824981
Mark Holmes <smlmrkhlms@gmail.com>
2017-07-14 14:00:48 -0400

    Updated State managment

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const logger = ({ collapsed }) => (prevState, nextState, updatedBy) => {
   console.groupEnd(title)
 }
 
-const App = store({ count: 0 }, [ logger({ collapsed: true }) ])(Counter)
+const App = store(
+  { count: 0 },
+  [ logger({ collapsed: true }) ]
+)(Counter)
 
 ReactDOM.render(<App hello="world" />, document.getElementById('root'))
