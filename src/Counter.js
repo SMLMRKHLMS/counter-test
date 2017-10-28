17b8ba74d896515b90c95d750f3578a173726520
Mark Holmes <smlmrkhlms@gmail.com>
2017-10-28 15:43:22 -0400

    Side Effect

--- src/Counter.js
+++ src/Counter.js
@@ -5,10 +5,12 @@ import withStore from './withStore'
 export default withStore(
   ({ count }) => ({ count }),
   {
+    alert: state => alert(JSON.stringify(state, null, 2)),
     decrement: ({ count }) => ({ count: count > 0 ? count - 1 : 0 }),
     increment: ({ count }) => ({ count: count + 1 })
   }
 )(function Counter({
+  alert,
   count,
   decrement,
   increment
@@ -16,6 +18,7 @@ export default withStore(
   return (
     <div>
       <div>{ count }</div>
+      <button onClick={ alert }>Alert</button>
       <button onClick={ decrement }>-</button>
       <button onClick={ increment }>+</button>
       <TodoApp />
