9088069963fbc49a76bd05e85b887b4463b44c22
Mark Holmes <smlmrkhlms@gmail.com>
2017-10-28 19:01:44 -0400

    Adding space

--- src/TodoApp/reducers.js
+++ src/TodoApp/reducers.js
@@ -3,14 +3,21 @@ export const add = ({ input, todos }) => input && ({
   todos: [...todos, { done: false, value: input }]
 })
 
-export const clear = () => ({ input: '', todos: [] })
+export const clear = () => ({
+  input: '', todos: []
+})
 
 export const remove = ({ todos }, index) => ({
   todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
 })
 
 export const toggle = ({ todos }, index) => ({
-  todos: todos.map((todo, i) => i === index ? { ...todo, done: !todo.done } : todo)
+  todos: todos.map((todo, i) => i === index
+    ? { ...todo, done: !todo.done }
+    : todo
+  )
 })
 
-export const update = (_, { currentTarget }) => ({ input: currentTarget.value })
+export const update = (_, { currentTarget }) => ({
+  input: currentTarget.value
+})
