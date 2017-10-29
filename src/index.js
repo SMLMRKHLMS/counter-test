b206486b91174bc11fed78dd33a0fa80b2199c68
Mark Holmes <smlmrkhlms@gmail.com>
2017-10-29 01:15:55 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { getItem, setItem } from './localStorage'
 
 const App = createStore(
   getItem('todos', { input: '', todos: [] }),
-  [setItem({ key: 'todos', saveIf: type => type !== 'update' })]
+  [
+    setItem({ key: 'todos', saveIf: type => type !== 'update' })
+  ]
 )(TodoApp)
 
 ReactDOM.render(<App />, document.getElementById('root'))
